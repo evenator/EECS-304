8339cab4b3902baec776ecb43bd01e863a2ce30e
Ed Venator <esv@case.edu>
2012-04-01 16:30:25 -0400

    Finished lab 4.

--- Lab 4/main.m
+++ Lab 4/main.m
@@ -12,19 +12,15 @@ p_den = conv(conv([1 0],[1 5]),[1 10]);
 p = tf(p_num, p_den)
 
 %1 Plot the RL without the controller
-%figure(1)
-%rlocus(p);
-%title('Root Locus of the Plant Without a Controller');
-
-%2 Design the controller
 Mp = .07;
 Ts = 3;
 Kv = 10;
 damping = sqrt(log(Mp)^2 / (pi^2 + log(Mp)^2))
 omega_n = 4 / (damping * Ts)
 %Plot parameters
-figure(2)
+figure(1)
 rlocus(p)
+title('Root Locus of the Plant Without Controller');
 hold on;
 omega_vec = -.1:-.1:-10;
 plot(omega_vec,omega_vec*(1-damping^2));
@@ -33,13 +29,18 @@ plot(-omega_n*damping*ones(1,11),(-5:1:5));
 hold off;
 %rltool(p);
 
-%return;
-k_c = 400;
-c_num = k_c * [1 10];
-c_den = [1 100];
-c = tf(c_num, c_den);
+
+%2 Design the controller
+k_c = 5;
+alpha = 10;
+T = .01;
+c_num = k_c * alpha * [T 1];
+c_den = [alpha * T 1];
+c = tf(c_num, c_den)
 sys = series(c,p);
+figure(2)
 rlocus(sys)
+title('Root Locus of the Plant With Controller');
 hold on;
 omega_vec = -.1:-.1:-10;
 plot(omega_vec,omega_vec*(1-damping^2),'r');
@@ -48,10 +49,11 @@ plot(-omega_n*damping*ones(1,11),(-5:1:5),'r');
 hold off;
 
 %3 Step Response
-sys_fb = feedback(sys,1);
+sys_fb = feedback(sys,1)
 figure(3);
-step(sys_fb);
+step_resp = step(sys_fb);
 title('Step Response');
+Mp = max(step_resp)-1
 
 %Ramp Response
 t_vec = 0:.1:100;
