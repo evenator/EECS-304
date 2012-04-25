8c2e76893c8cabe7c67757e7cea6f40c350d9f4e
Ed Venator <esv@case.edu>
2012-04-25 07:43:41 -0400

    Helicopter test files for assorted boundary cases.

--- Helicopter/main.m
+++ Helicopter/main.m
@@ -2,27 +2,70 @@
 %EECS 304 Spring 2012
 %Helicopter Project
 
+J_list = zeros(16, 1);
+
 %System Dynamics
-k = 0.15;
-gi = 0.05;
-wn = 1.25;
-T = 0.09;
+T = .09
+wn = 1.0;
+gi = .03;
+k = .1
+run_helicopter;
+J_list(1) = J;
+k = .15
+run_helicopter;
+J_list(2) = J;
+gi = .05;
+k = .1
+run_helicopter;
+J_list(3) = J;
+k = .15
+run_helicopter;
+J_list(4) = J;
+wn = 1.5;
+gi = .03;
+k = .1
+run_helicopter;
+J_list(5) = J;
+k = .15
+run_helicopter;
+J_list(6) = J;
+gi = .05;
+k = .1
+run_helicopter;
+J_list(7) = J;
+k = .15
+run_helicopter;
+J_list(8) = J;
+T = 0.11;
+wn = 1.0;
+gi = .03;
+k = .1
+run_helicopter;
+J_list(9) = J;
+k = .15
+run_helicopter;
+J_list(10) = J;
+gi = .05;
+k = .1
+run_helicopter;
+J_list(11) = J;
+k = .15
+run_helicopter;
+J_list(12) = J;
+wn = 1.5;
+gi = .03;
+k = .1
+run_helicopter;
+J_list(13) = J;
+k = .15
+run_helicopter;
+J_list(14) = J;
+gi = .05;
+k = .1
+run_helicopter;
+J_list(15) = J;
+k = .15
+run_helicopter;
+J_list(16) = J;
 
-%Prefilter
-numF = 1;
-denF = 1;
-
-%Controller
-%PID
-kp = 50;
-kd = 2.5 * kp;
-ki = .7 * kp;
-lp = 90;
-numG = [kd kp ki];
-denG = [1/lp 1 0];
-
-%Run Simulation
-sim('helicopter1');
-
-%Calculate Score
-[components, J] = cf(t, u, r, y)
+J_list
---
+++ Helicopter/run_helicopter.m
@@ -0,0 +1,22 @@
+%Edward Venator
+%EECS 304 Spring 2012
+%Helicopter Project
+
+%Prefilter
+numF = 1;
+denF = 1;
+
+%Controller
+%PID
+kp = 50;
+kd = 2.5 * kp;
+ki = .7 * kp;
+lp = 90;
+numG = [kd kp ki];
+denG = [1/lp 1 0];
+
+%Run Simulation
+sim('helicopter1');
+
+%Calculate Score
+[components, J] = cf(t, u, r, y);
